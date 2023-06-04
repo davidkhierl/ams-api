60a1e4295a3cbab65bc445772d140422277c253c
David Khierl <dkrgidor@gmail.com>
2023-06-04 23:37:08 +0400

    fix: refresh error code

--- src/auth/guards/jwt-refresh-auth.guard.ts
+++ src/auth/guards/jwt-refresh-auth.guard.ts
@@ -1,5 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtRefreshAuthGuard extends AuthGuard('jwt-refresh') {}
+export class JwtRefreshAuthGuard extends AuthGuard('jwt-refresh') {
+  handleRequest(err, user) {
+    if (err || !user) {
+      throw err || new ForbiddenException();
+    }
+
+    return user;
+  }
+}
